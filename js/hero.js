8e1332b2e13ac351e386d6e598de5be635cae4e2
DoaaOsama <doaa_kandeel@hotmail.com>
2019-01-04 10:28:13 +0200

    Jump added to hero, hero component added

--- js/hero.js
+++ js/hero.js
@@ -1,8 +1,6 @@
 
-class gameObject
-{
-    constructor(animateInterval,stripeURLs,stripeOffset,startSliceOffset,stripeEnds)
-    {
+class gameObject {
+    constructor(animateInterval, stripeURLs, stripeOffset, startSliceOffset, stripeEnds) {
         this.animateInterval = animateInterval;
         this.stripeURLs = stripeURLs;
         this.stripeOffset = stripeOffset;
@@ -11,18 +9,17 @@ class gameObject
     }
 }
 
-class Hero extends gameObject
-{
-    constructor(animateInterval,stripeURLs,stripeOffset,startSliceOffset,stripeEnds,heroCharacter)
-    {
-        super(animateInterval,stripeURLs,stripeOffset,startSliceOffset,stripeEnds);
+class Hero extends gameObject {
+    constructor(animateInterval, stripeURLs, stripeOffset, startSliceOffset, stripeEnds, heroCharacter) {
+        super(animateInterval, stripeURLs, stripeOffset, startSliceOffset, stripeEnds);
         this.clearRunInterval;
         this.firstRun = true; // acts if the game is running for the first time or after game over
         this.heroCharacter = heroCharacter;
-        
+        this.isJumping = false;
+        this.topPosition=256;
+        this.isShooting = false;
     }
-    stopRunning()
-    {
+    stopRunning() {
         window.clearInterval(this.clearRunInterval);
     }
     startRunning() //stripeOffset,startSliceOffset,animateInterval,
@@ -47,26 +44,74 @@ class Hero extends gameObject
         // }
         // else
         // {
-            // debugger;
-            // console.log(this.stripeURLs);
-            this.clearRunInterval = window.setInterval(()=>{
-                this.heroCharacter.style.backgroundImage = "url("+this.stripeURLs.run+")";
-                this.heroCharacter.style.backgroundPosition = (-1*this.startSliceOffset)+'px 0px';
-                // console.log(this.startSliceOffset);
-                if (this.startSliceOffset < this.stripeEnds.run) {
+        // debugger;
+        // console.log(this.stripeURLs);
+        this.clearRunInterval = window.setInterval(() => {
+            this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.run + ")";
+            this.heroCharacter.style.backgroundPosition = (-1 * this.startSliceOffset) + 'px 256px';
+            // console.log(this.startSliceOffset);
+            if (this.startSliceOffset < this.stripeEnds.run) {
+                this.startSliceOffset = this.startSliceOffset + this.stripeOffset;
+            }
+            else {
+                this.startSliceOffset = this.stripeOffset;
+            }
+        }, this.animateInterval);
+        // }
+    }
+
+    startJumping() {
+        if (this.isJumping == false) {
+            this.stopRunning();
+            this.isJumping = true;
+            this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.jump + ")";
+            var imageNumber = 0;
+            this.startSliceOffset = this.stripeOffset;
+            this.clearRunInterval = window.setInterval(() => {
+                this.heroCharacter.style.backgroundPosition = (-1 * this.startSliceOffset) + 'px 256px';
+                if (this.startSliceOffset < 3000) {
                     this.startSliceOffset = this.startSliceOffset + this.stripeOffset;
-                  }
+                    imageNumber++;
+                    if (imageNumber <= 5) {
+                        this.topPosition -= 40;
+                        this.heroCharacter.style.top = this.topPosition + "px";
+                    }
+                    else {
+                        this.topPosition += 50;
+                        this.heroCharacter.style.top = this.topPosition + "px";
+                    }
+                }
                 else {
                     this.startSliceOffset = this.stripeOffset;
-                  }
-            },this.animateInterval);
-        // }
+                    imageNumber = 0;
+                    this.topPosition = 256;
+                    this.heroCharacter.style.top = this.topPosition + "px";
+                    this.stopRunning();
+                    this.isJumping = false;
+                    this.startRunning();
+                }
+            }, this.animateInterval);
+        }
+    }
+
+    shoot(){
+        this.stopRunning();
+        if(this.isJumping == false && this.isShooting == false){
+
+        }
     }
 }
 var x = {
-    run:'images/Run-Stripe.png',
-    idle: 'images/idle.png'
-        };
+    run: 'images/Run-Stripe.png',
+    idle: 'images/idle.png',
+    jump: 'images/jump.png',
+    shoot: 'images/shoot-Stripe.png'
+};
 console.log(x.run);
-var hero = new Hero(70,x,300,0,{run:2400}, document.getElementById("hero"))
+var hero = new Hero(70, x, 300, 0, { run: 2400 }, document.getElementById("hero"))
 hero.startRunning();
+window.onkeyup = function () {
+    if (event.keyCode == 32) {
+        hero.startJumping();
+    };
+}
