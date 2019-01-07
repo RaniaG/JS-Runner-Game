4fa12593278959184b611a34da2a5875aac86512
DoaaOsama <doaa_kandeel@hotmail.com>
2019-01-07 22:27:22 +0200

    removed comments

--- js/trial.js
+++ js/trial.js
@@ -8,47 +8,6 @@ var e2 = new Enemy(roadTop);
 e2.draw();
 e2.animate();
 
-
-
-
-// var interval = window.setInterval(() => {
-//     // debugger;
-//     var heroboundingrect = hero.heroCharacter.getBoundingClientRect();
-//     var enemyboundingrect = e2.div.getBoundingClientRect();
-//     console.log(heroboundingrect.right, enemyboundingrect.left)
-//     console.log(heroboundingrect.top, enemyboundingrect.bottom)
-//     console.log(heroboundingrect.bottom, enemyboundingrect.top)
-//     console.log("------------------------");
-
-//     var checkOverlap ;
-//     var reason;
-//     // = !(heroboundingrect.right < enemyboundingrect.left + 110 ||
-//     //     heroboundingrect.left > enemyboundingrect.right ||
-//     //     (heroboundingrect.bottom < enemyboundingrect.top && heroboundingrect.bottom > enemyboundingrect.bottom));
-
-//     if(hero.isJumping){
-//         checkOverlap = !((heroboundingrect.right < enemyboundingrect.left + 110 || heroboundingrect.left > enemyboundingrect.right+60 ) ||
-//             (heroboundingrect.top > enemyboundingrect.top &&
-//             heroboundingrect.bottom > enemyboundingrect.bottom )
-//             )
-//             reason = "died while jumping";
-
-//     }
-//     else{
-//         checkOverlap = !(heroboundingrect.right < enemyboundingrect.left + 110)
-//         reason = "not JUmping";
-//     }
-
-//     if (checkOverlap) {
-//         console.log("overlapped " + reason);
-//         destroy(e2);
-//         hero.dead();
-//         hero.lives = 0;
-//         window.clearInterval(interval);
-
-//     }
-// }, 50);
-
 interval = window.setInterval(() => {
     // debugger;
     var heroboundingrect = hero.heroCharacter.getBoundingClientRect();
