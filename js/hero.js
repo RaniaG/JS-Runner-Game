8e103022bdd096b61821a8a837dc154a573bb0d7
DoaaOsama <doaa_kandeel@hotmail.com>
2019-01-10 21:03:11 +0200

    collision detected

--- js/hero.js
+++ js/hero.js
@@ -9,7 +9,7 @@ class gameObject {
 }
 
 //roadTop 260px
-class Hero extends gameObject {    
+class Hero extends gameObject {
     constructor(animateInterval, stripeURLs, stripeOffset, startSliceOffset, stripeEnds, heroCharacter) {
         super(animateInterval, stripeURLs, stripeOffset, startSliceOffset, stripeEnds);
         this.clearAnimateInterval;
@@ -18,13 +18,13 @@ class Hero extends gameObject {
         this.isJumping = false;
         // clientHeight is an element height including padding but not bordering 
         this.top = (parseInt(roadTop) - this.heroCharacter.clientHeight); // to place hero above road
-        this.heroCharacter.style.top = this.top+"px";
+        this.heroCharacter.style.top = this.top + "px";
         this.topPosition = this.top;
         this.isShooting = false;
         this.shoots = false;
         this.highJump = false;
         this.lives = 3; //
-        this.coins ={//
+        this.coins = {//
             gold: 0,
             silver: 0
         };
@@ -43,27 +43,27 @@ class Hero extends gameObject {
     startRunning() //stripeOffset,startSliceOffset,animateInterval,
     {
         {
-        // if(this.firstRun)
-        // {
-        //     heroCharacter.style.backgroundImage = "url("+this.stripeURLs.idle+")";
-        //     //some code to fall from above
-        //     var top = 0;
-        //     heroCharacter.style.top = top+'px';
-        //     do {
-        //         window.setInterval(()=>
-        //         {
-        //             top+=10;
-        //     heroCharacter.style.top = top+'px';
+            // if(this.firstRun)
+            // {
+            //     heroCharacter.style.backgroundImage = "url("+this.stripeURLs.idle+")";
+            //     //some code to fall from above
+            //     var top = 0;
+            //     heroCharacter.style.top = top+'px';
+            //     do {
+            //         window.setInterval(()=>
+            //         {
+            //             top+=10;
+            //     heroCharacter.style.top = top+'px';
 
-        //         },animateInterval);
-        //     } while (top<window.innerHeight);
-        //     this.firstRun = false;
-        //     this.startRunning(2400);
-        // }
-        // else
-        // {
-        // debugger;
-        // console.log(this.stripeURLs);
+            //         },animateInterval);
+            //     } while (top<window.innerHeight);
+            //     this.firstRun = false;
+            //     this.startRunning(2400);
+            // }
+            // else
+            // {
+            // debugger;
+            // console.log(this.stripeURLs);
         }
         this.clearAnimateInterval = window.setInterval(() => {
             this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.run + ")";
@@ -121,7 +121,7 @@ class Hero extends gameObject {
             this.stopCurrentAnimation();
             this.startSliceOffset = this.stripeOffset;
             this.isShooting = true;
-            this.shoots=true;
+            this.shoots = true;
             this.isJumping = true;
             this.clearAnimateInterval = window.setInterval(() => {
                 this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.shoot + ")";
@@ -137,22 +137,20 @@ class Hero extends gameObject {
                     this.isJumping = false;
                     this.startRunning();
                 }
-            }, this.animateInterval*0.5);
+            }, this.animateInterval * 0.5);
 
         }
     }
 
-    clickHighJump()
-    {
-        if (this.isJumping && !this.highJump && this.topPosition < this.top)
-        {
+    clickHighJump() {
+        if (this.isJumping && !this.highJump && this.topPosition < this.top) {
             this.stopCurrentAnimation();
             this.isJumping = true;
             this.highJump = true;
             this.isShooting = true;
             this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.jump + ")";
             var imageNumber = 0;
-            var distanceFromGround=this.top-this.topPosition;
+            var distanceFromGround = this.top - this.topPosition;
             console.log(distanceFromGround);
 
             this.startSliceOffset = this.stripeOffset;
@@ -163,11 +161,11 @@ class Hero extends gameObject {
                     imageNumber++;
                     if (imageNumber <= 5) {
                         this.topPosition -= 40;
-                        distanceFromGround+=40;
+                        distanceFromGround += 40;
                         this.heroCharacter.style.top = this.topPosition + "px";
                     }
                     else {
-                        this.topPosition += (distanceFromGround/4);
+                        this.topPosition += (distanceFromGround / 4);
                         this.heroCharacter.style.top = this.topPosition + "px";
                     }
                 }
@@ -186,23 +184,23 @@ class Hero extends gameObject {
         }
     }
 
-    dead(){
+    dead() {
         this.stopCurrentAnimation();
-            this.startSliceOffset = this.stripeOffset;
-            this.isShooting = true;
-            this.isJumping = true;
-            this.highJump = true;
-            this.clearAnimateInterval = window.setInterval(() => {
-                this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.deadSprite + ")";
-                this.heroCharacter.style.backgroundPosition = (-1 * this.startSliceOffset) + 'px 254px';
-                this.heroCharacter.style.top = this.top+"px";
-                if (this.startSliceOffset < this.stripeEnds.dead - this.stripeOffset) {
-                    this.startSliceOffset = this.startSliceOffset + this.stripeOffset;
-                }
-                else {
-                    this.stopCurrentAnimation(); 
-                }
-            }, this.animateInterval *1.25);
+        this.startSliceOffset = this.stripeOffset;
+        this.isShooting = true;
+        this.isJumping = true;
+        this.highJump = true;
+        this.clearAnimateInterval = window.setInterval(() => {
+            this.heroCharacter.style.backgroundImage = "url(" + this.stripeURLs.deadSprite + ")";
+            this.heroCharacter.style.backgroundPosition = (-1 * this.startSliceOffset) + 'px 254px';
+            this.heroCharacter.style.top = this.top + "px";
+            if (this.startSliceOffset < this.stripeEnds.dead - this.stripeOffset) {
+                this.startSliceOffset = this.startSliceOffset + this.stripeOffset;
+            }
+            else {
+                this.stopCurrentAnimation();
+            }
+        }, this.animateInterval * 1.25);
     }
 
     // crash(object)
@@ -212,8 +210,7 @@ class Hero extends gameObject {
     //         this.crash(generatedObjects[index])
     //         }
     // }
-    crash()
-    {
+    crash() {
         var crashReturn = false;
         // this.currentTopPos = this.heroCharacter.getBoundingClientRect().top;
         // this.currentBottomPos = this.heroCharacter.getBoundingClientRect().bottom;
@@ -229,116 +226,136 @@ class Hero extends gameObject {
         // objCurrentLeftPos = objectHit.getBoundingClientRect().left;
         // objCurrentRightPos = objCurrentLeftPos + objectHit.getBoundingClientRect().width;
         //if collectable
-            //if heart
-            //if coin
+        //if heart
+        //if coin
         //if obstacle
-            //if to end game
-            // if to reduce lives
-            // if(this.currentBottomPos > objCurrentTopPos || this.cur < objCurrentTopPos)
-             //#region inne Condition of crash boundaries0
-        var possibleHits=[];
-        console.log(this.heroCharacter);
+        //if to end game
+        // if to reduce lives
+        // if(this.currentBottomPos > objCurrentTopPos || this.cur < objCurrentTopPos)
+        //#region inne Condition of crash boundaries0
+        var possibleHits = [];
         var currentBoundingBox = this.heroCharacter.getBoundingClientRect();
-        console.log(currentBoundingBox.top);
-        var heroActualTop =currentBoundingBox.top+20;
-        var heroActualLeft =(currentBoundingBox.left+50);
-        console.log(heroActualTop);
-        possibleHits.push(document.elementsFromPoint(heroActualLeft+75, (heroActualTop+111))[1]);
+        var heroActualTop = currentBoundingBox.top + 20;
+        var heroActualLeft = (currentBoundingBox.left + 50);
+        // console.log(document.elementsFromPoint(heroActualLeft+75, (heroActualTop+111)));
+        possibleHits.push(document.elementsFromPoint(heroActualLeft + 75, (heroActualTop + 111))[1]);
         // possibleHits.push(document.elementsFromPoint(heroActualLeft, (heroActualTop+111))[1]);
         // possibleHits.push(document.elementsFromPoint((heroActualLeft+37), heroActualTop)[1]);
         // possibleHits.push(document.elementsFromPoint((heroActualLeft+112), heroActualTop)[1]);
-        
-        console.log(possibleHits)
-        console.log((heroActualTop+55));
 
         for (let index = 0; index < possibleHits.length; index++) {
-            if(possibleHits[index].classList.contains("collectable--heart"))
-            {
+            if (possibleHits[index].classList.contains("collectable--heart")) {
                 this.updateLives(true);
+                possibleHits[index].remove;
             }
-            else if(possibleHits[index].classList.contains("obstacle--cactus--1") || possibleHits[index].classList.contains("obstacle--cactus--2")
-            || possibleHits[index].classList.contains("obstacle--rock--1") || possibleHits[index].classList.contains("obstacle--rock--5")
-            || possibleHits[index].classList.contains("obstacle--rock--2") || possibleHits[index].classList.contains("obstacle--rock--3")
-            || possibleHits[index].classList.contains("obstacle--rock--4")  )
-            {
+            // else if(possibleHits[index].classList.contains("obstacle--cactus--1") || possibleHits[index].classList.contains("obstacle--cactus--2")
+            // || possibleHits[index].classList.contains("obstacle--rock--1") || possibleHits[index].classList.contains("obstacle--rock--5")
+            // || possibleHits[index].classList.contains("obstacle--rock--2") || possibleHits[index].classList.contains("obstacle--rock--3")
+            // || possibleHits[index].classList.contains("obstacle--rock--4")  )
+            // {
+            //     this.updateLives(false);
+
+            // }
+            else if (possibleHits[index].classList.contains("obstacle--cactus--1")) {
+                debugger;
                 this.updateLives(false);
+                possibleHits[index].classList.remove("obstacle--cactus--1");
+                possibleHits[index].classList.add("destroy--cactus");
+
             }
-            else if(possibleHits[index].classList.contains("obstacle--enemy")){
+            else if (possibleHits[index].classList.contains("obstacle--rock--5")) {
+                this.updateLives(false);
+                possibleHits[index].classList.remove("obstacle--rock--5");
+                possibleHits[index].classList.add("destroy--rock");
+
+            }
+
+            else if (possibleHits[index].classList.contains("obstacle--enemy")) {
                 this.endGame();
             }
-            else if(possibleHits[index].classList.contains("collectable--coin--gold")){
-                this.updateCoins("gold")
+            else if (possibleHits[index].classList.contains("collectable--coin__face")) {
+                if (possibleHits[index].parentElement.classList.contains("collectable--coin--gold")) {
+                    this.updateCoins("gold");
+                    possibleHits[index].remove;
+                }
+                else if (possibleHits[index].parentElement.classList.contains("collectable--coin--silver")) {
+                    this.updateCoins("silver");
+                    possibleHits[index].remove;
+                }
             }
-            else if(possibleHits[index].classList.contains("collectable--coin--silver")){
-                this.updateCoins("silver")
-            }
-            
+
         }
 
-            //  var overlappedHorizontal = (
-            //     // (this.currentBoundingBox.right == objCurrentBoundingBox.left + 110 && this.currentBoundingBox.left < objCurrentBoundingBox.left) ||
-            //     (this.currentBoundingBox.right >= objCurrentBoundingBox.left + 110 && this.currentBoundingBox.right <= objCurrentBoundingBox.right + 110 && this.currentBoundingBox.left+50 <= objCurrentBoundingBox.left) ||
-            //     (this.currentBoundingBox.right > objCurrentBoundingBox.right + 110 && this.currentBoundingBox.left+50 <= objCurrentBoundingBox.right && this.currentBoundingBox.left+50 > objCurrentBoundingBox.left)
-            //     );
-            // var overlappedVertical = (
-            //     this.currentBoundingBox.bottom < objCurrentBoundingBox.bottom && this.currentBoundingBox.bottom >= objCurrentBoundingBox.top ||
-            //     this.currentBoundingBox.top > objCurrentBoundingBox.top && this.currentBoundingBox.top <= objCurrentBoundingBox.bottom
-            // );
+        //  var overlappedHorizontal = (
+        //     // (this.currentBoundingBox.right == objCurrentBoundingBox.left + 110 && this.currentBoundingBox.left < objCurrentBoundingBox.left) ||
+        //     (this.currentBoundingBox.right >= objCurrentBoundingBox.left + 110 && this.currentBoundingBox.right <= objCurrentBoundingBox.right + 110 && this.currentBoundingBox.left+50 <= objCurrentBoundingBox.left) ||
+        //     (this.currentBoundingBox.right > objCurrentBoundingBox.right + 110 && this.currentBoundingBox.left+50 <= objCurrentBoundingBox.right && this.currentBoundingBox.left+50 > objCurrentBoundingBox.left)
+        //     );
+        // var overlappedVertical = (
+        //     this.currentBoundingBox.bottom < objCurrentBoundingBox.bottom && this.currentBoundingBox.bottom >= objCurrentBoundingBox.top ||
+        //     this.currentBoundingBox.top > objCurrentBoundingBox.top && this.currentBoundingBox.top <= objCurrentBoundingBox.bottom
+        // );
 
-            // if (overlappedHorizontal) {
-            //     if (this.isJumping) {
-            //         if (overlappedVertical) {
-            //             //console.log(heroboundingrect.bottom, crashedboundingrect.top);
-            //             crashReturn = true;
-            //         }
-            //     }
-            //     else{
-            //     crashReturn = true;
-            //     }
-            // }
-            // return crashReturn;
+        // if (overlappedHorizontal) {
+        //     if (this.isJumping) {
+        //         if (overlappedVertical) {
+        //             //console.log(heroboundingrect.bottom, crashedboundingrect.top);
+        //             crashReturn = true;
+        //         }
+        //     }
+        //     else{
+        //     crashReturn = true;
+        //     }
+        // }
+        // return crashReturn;
 
-            //  #endregion
+        //  #endregion
     }
     updateLives(extraLife) //
     {
         //is extraLife true then add one else --
         var lives = document.getElementsByClassName("icon--heart")[0];
-        if(extraLife)
-        {
-            this.lives ++;      
+        if (extraLife) {
+            this.lives++;
         }
-        else
-        {
-            this.lives--;
+        else {
+            if (this.lives > 0)
+                this.lives--;
+            else
+                this.endGame();
         }
-        lives.nextElementSibling.innerHTML = "x"+this.lives;
+        lives.innerHTML = "x" + this.lives;
     }
     updateCoins(type) //
     {
-        if(type === "gold"){
-            this.coins.gold ++;
-            document.getElementsByClassName("icon--coin--gold")[0].innerHTML = "x"+this.coins.gold ;
+        if (type === "gold") {
+            this.coins.gold++;
+            document.getElementsByClassName("icon--coin--gold")[0].innerHTML = "x" + this.coins.gold;
             this.updateMilage(500);
+            console.log("gold " + this.coins.gold);
         }
-        else if(type === "silver"){
-            this.coins.silver ++;
-            document.getElementsByClassName("icon--coin--silver")[0].innerHTML = "x"+this.coins.silver ;
+        else if (type === "silver") {
+            this.coins.silver++;
+            document.getElementsByClassName("icon--coin--silver")[0].innerHTML = "x" + this.coins.silver;
             this.updateMilage(100);
+            console.log("silver " + this.coins.silver);
+
         }
 
     }
     updateMilage(milageIncrease) //
     {
         this.milage += milageIncrease;
-        document.getElementsByClassName("icon--coin--run")[0].innerHTML = "x"+this.milage ;
+        document.getElementsByClassName("icon--run")[0].innerHTML = "x" + this.milage;
+        console.log("milage " + this.milage);
+
     }
     endGame() //
     {
         this.dead();
-        this.gameOver = true; 
+        this.gameOver = true;
     }
- }
+}
 var x = {
     run: 'images/Run-Stripe.png',
     idle: 'images/idle.png',
@@ -348,29 +365,28 @@ var x = {
     dead: 'images/dead2.png'
 };
 console.log(x.run);
-var hero = new Hero(70, x, 300, 0, { run: 2400, jump:3000, shoot: 900, dead: 3000  }, document.getElementById("hero"))
+var hero = new Hero(70, x, 300, 0, { run: 2400, jump: 3000, shoot: 900, dead: 3000 }, document.getElementById("hero"))
 hero.startRunning();
 
 window.onkeydown = function (event) {
-    switch(event.keyCode)
-    {
+    switch (event.keyCode) {
         case 32: // space
-        hero.startJumping();
-        break;
+            hero.startJumping();
+            break;
         case 13: // enter
-        hero.shoot();
-        break;
+            hero.shoot();
+            break;
         case 38: // up arrow
-        hero.clickHighJump();
-        break;
+            hero.clickHighJump();
+            break;
         default:
-        // left 37 in case fast forward
-        // esc 27 in case pause
-        break;
+            // left 37 in case fast forward
+            // esc 27 in case pause
+            break;
     }
 }
 // hero.updateLives(true);
-setInterval(function(){
+setInterval(function () {
     hero.crash();
-},50)
+}, 50)
 // document.getElementsByTagName("body")[0].addEventListener("click",hero.clickHighJump);
