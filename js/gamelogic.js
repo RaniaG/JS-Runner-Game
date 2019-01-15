ef7f8bf59ca981f9d04a1468f8d3a6c1db65f150
BassantFahmy <bassantfahmy@gmail.com>
2019-01-15 23:14:16 +0200

    printscorefunction

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -5,39 +5,39 @@ class Gamelogic {
             gold: 0,
             silver: 0
         };
-        this.milage=0;
+        this.milage = 0;
         this.gameOver = false;
         this.generation = new Generation();
-        this.backMusic=new Sound(7);
-        this.hero = new Hero(70, x, 300, 0, { run: 2400, jump:3000, shoot: 900, dead: 3000  }, document.getElementById("hero"));
+        this.backMusic = new Sound(7);
+        this.hero = new Hero(70, x, 300, 0, { run: 2400, jump: 3000, shoot: 900, dead: 3000 }, document.getElementById("hero"));
     }
 
-    startGame(){
+    startGame() {
         this.backMusic.play();
-        setInterval(()=>{
+        setInterval(() => {
             this.backMusic.play();
-        },121000);
+        }, 121000);
         this.generation.start();
         this.hero.startRunning();
         this.inputHandler();
         setInterval(function (game) {
             game.crash();
             game.updateMilage(10);
-        }, 10,this)
+        }, 10, this)
         setInterval
     }
-    inputHandler(){
-        var self=this;
+    inputHandler() {
+        var self = this;
         window.onkeydown = function (event) {
             switch (event.keyCode) {
                 case 32: // space
-                self.hero.startJumping();
+                    self.hero.startJumping();
                     break;
                 case 13: // enter
-                self.hero.shoot();
+                    self.hero.shoot();
                     break;
                 case 38: // up arrow
-                self.hero.clickHighJump();
+                    self.hero.clickHighJump();
                     break;
                 default:
                     // left 37 in case fast forward
@@ -52,15 +52,15 @@ class Gamelogic {
         var heroActualTop = currentBoundingBox.top + 20; //startof the hat 
         var heroActualLeft = (currentBoundingBox.left + 50); // start of the hero left
 
-         possibleHitsPoints.push([heroActualLeft,heroActualTop]);
-         possibleHitsPoints.push([heroActualLeft+150,heroActualTop]);
-         possibleHitsPoints.push([heroActualLeft,heroActualTop+222]);
-         possibleHitsPoints.push([heroActualLeft+150,heroActualTop+222]);
-         possibleHitsPoints.push([heroActualLeft+75,heroActualTop+111]);
-         possibleHitsPoints.push([heroActualLeft+30,heroActualTop+55]);
-         possibleHitsPoints.push([heroActualLeft+90,heroActualTop+167]);
+        possibleHitsPoints.push([heroActualLeft, heroActualTop]);
+        possibleHitsPoints.push([heroActualLeft + 150, heroActualTop]);
+        possibleHitsPoints.push([heroActualLeft, heroActualTop + 222]);
+        possibleHitsPoints.push([heroActualLeft + 150, heroActualTop + 222]);
+        possibleHitsPoints.push([heroActualLeft + 75, heroActualTop + 111]);
+        possibleHitsPoints.push([heroActualLeft + 30, heroActualTop + 55]);
+        possibleHitsPoints.push([heroActualLeft + 90, heroActualTop + 167]);
         for (let index = 0; index < possibleHitsPoints.length; index++) {
-            var possibleHits = document.elementsFromPoint(possibleHitsPoints[index][0],possibleHitsPoints[index][1])[1];
+            var possibleHits = document.elementsFromPoint(possibleHitsPoints[index][0], possibleHitsPoints[index][1])[1];
             if (possibleHits.classList.contains("collectable--heart")) {
                 this.updateLives(true);
                 this.hitHeart(possibleHits);
@@ -86,21 +86,19 @@ class Gamelogic {
                     this.hitCoin(possibleHits);
                 }
             }
-            
+
 
         }
     }
 
-    updateLives(extraLife) 
-    {
+    updateLives(extraLife) {
         //is extraLife true then add one else --
         var lives = document.getElementById("lives");
         if (extraLife) {
             this.lives++;
         }
         else {
-            if (this.lives > 1)
-            {
+            if (this.lives > 1) {
                 this.lives--;
             }
             else
@@ -132,51 +130,52 @@ class Gamelogic {
         console.log("milage " + this.milage);
 
     }
+
     endGame() //
     {
         this.hero.dead();
-        window.location.href = 'gameover.html?goldcoins='+this.coins.gold+"&silvercoins="+this.coins.silver+"&miles="+this.milage;        
+        window.location.href = 'gameover.html?goldcoins=' + this.coins.gold + "&silvercoins=" + this.coins.silver + "&miles=" + this.milage;
         this.gameOver = true;
+
     }
-    hitRock(x){
-        var heroHitSound=new Sound(2);
+    hitRock(x) {
+        var heroHitSound = new Sound(2);
         heroHitSound.play();
-        var left=window.getComputedStyle(x).getPropertyValue("left");
-        x.className="destroy destroy--rock";
-        x.style.animationName="rock-destroy";
-        x.style.left=left;
-        
-    
+        var left = window.getComputedStyle(x).getPropertyValue("left");
+        x.className = "destroy destroy--rock";
+        x.style.animationName = "rock-destroy";
+        x.style.left = left;
+
+
     }
-    hitCactus(x){
-        var heroHitSound=new Sound(2);
+    hitCactus(x) {
+        var heroHitSound = new Sound(2);
         heroHitSound.play();
-        var left=window.getComputedStyle(x).getPropertyValue("left");
-        x.className="destroy destroy--cactus";
-        x.style.animationName="cactus-destroy";
-        x.style.left=left;
-        
+        var left = window.getComputedStyle(x).getPropertyValue("left");
+        x.className = "destroy destroy--cactus";
+        x.style.animationName = "cactus-destroy";
+        x.style.left = left;
+
     }
-    hitEnemy(){
-        var enemy=document.querySelector(".obstacle--enemy");
-        if(enemy&&enemy.offsetLeft<1200)
-        {
+    hitEnemy() {
+        var enemy = document.querySelector(".obstacle--enemy");
+        if (enemy && enemy.offsetLeft < 1200) {
             enemy.remove();
-            var trollDieSound = new Sound(5); 
-            trollDieSound.play(); 
+            var trollDieSound = new Sound(5);
+            trollDieSound.play();
         }
     }
-    hitCoin(x){
-        var coinSound=new Sound(0);
+    hitCoin(x) {
+        var coinSound = new Sound(0);
         coinSound.play();
         console.log(x.parentElement);
         x.parentElement.remove();
     }
-    hitHeart(x){
-        var heartSound=new Sound(6);
+    hitHeart(x) {
+        var heartSound = new Sound(6);
         heartSound.play();
         x.remove();
     }
 }
-var game=new Gamelogic();
+var game = new Gamelogic();
 game.startGame();
--- js/gameover.js
+++ js/gameover.js
@@ -1,24 +1,18 @@
-const urlParams = new URLSearchParams(window.location.search);
-const goldcoins = urlParams.get('goldcoins');
-const silvercoins = urlParams.get('silvercoins');
-const miles = parseInt(urlParams.get('miles'));
-var text = document.querySelectorAll(".gameover__text");
-var highScore = parseInt(localStorage.getItem("score"));
-
 function printScore() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const goldcoins = urlParams.get('goldcoins');
+    const silvercoins = urlParams.get('silvercoins');
+    const miles = parseInt(urlParams.get('miles'));
+    var text = document.querySelectorAll(".gameover__text");
+    var highScore = parseInt(localStorage.getItem("score"));
     if (isNaN(highScore)) {
         localStorage.setItem("score", miles);
     }
-
     if (miles > highScore) {
         localStorage.setItem("score", miles);
     }
-
     highScore = localStorage.getItem("score");
-
     var arr = [goldcoins, silvercoins, miles, highScore];
-
-
     text.forEach((el, i) => {
         el.innerHTML += " " + arr[i];
     })
