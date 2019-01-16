ae54303aad8d4061393c93d8ac147316209598f0
BassantFahmy <bassantfahmy@gmail.com>
2019-01-16 13:12:17 +0200

    classGameOver

--- js/gameover.js
+++ js/gameover.js
@@ -1,20 +1,31 @@
-function printScore() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const goldcoins = urlParams.get('goldcoins');
-    const silvercoins = urlParams.get('silvercoins');
-    const miles = parseInt(urlParams.get('miles'));
-    var text = document.querySelectorAll(".gameover__text");
-    var highScore = parseInt(localStorage.getItem("score"));
-    if (isNaN(highScore)) {
-        localStorage.setItem("score", miles);
+class gameOver {
+    constructor() {
+     
+
+    };
+    printScore() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const goldcoins = urlParams.get('goldcoins');
+        const silvercoins = urlParams.get('silvercoins');
+        const miles = parseInt(urlParams.get('miles'));
+        var text = document.querySelectorAll(".gameover__text");
+        var highScore = parseInt(localStorage.getItem("score"));
+        if (isNaN(highScore)) {
+            localStorage.setItem("score", miles);
+        }
+        if (miles > highScore) {
+            localStorage.setItem("score", miles);
+        }
+        highScore = localStorage.getItem("score");
+        var arr = [goldcoins, silvercoins, miles, highScore];
+        text.forEach((el, i) => {
+            el.innerHTML += " " + arr[i];
+        })
     }
-    if (miles > highScore) {
-        localStorage.setItem("score", miles);
-    }
-    highScore = localStorage.getItem("score");
-    var arr = [goldcoins, silvercoins, miles, highScore];
-    text.forEach((el, i) => {
-        el.innerHTML += " " + arr[i];
-    })
+
 }
-printScore();
+
+
+
+var display = new gameOver();
+display.printScore();
